848154a1bc113a2f5486e32412a6612b84627ca7
Saravanan <msx3gamer1@gmail.com>
2026-04-10 20:38:56 +0530

    Add initial SQLite database setup and sample data insertion

---
+++ sqlite.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+connection=sqlite3.connect('studentdb.db')
+
+cursor=connection.cursor()
+
+table_info="""
+CREATE TABLE IF NOT EXISTS students(NAME VARCHAR(20),CLASS VARCHAR(20),
+AGE INT);
+"""
+
+cursor.execute(table_info)
+
+cursor.execute('''INSERT INTO students VALUES("John","10th",15)''')
+cursor.execute('''INSERT INTO students VALUES("Alice","9th",14)''')
+cursor.execute('''INSERT INTO students VALUES("Bob","11th",16)''')  
+cursor.execute('''INSERT INTO students VALUES("Eve","10th",15)''')
+cursor.execute('''INSERT INTO students VALUES("Charlie","9th",14)''')
+
+print("Data inserted are:")
+data=cursor.execute('''SELECT * FROM students''')
+for row in data:
+    print(row)
