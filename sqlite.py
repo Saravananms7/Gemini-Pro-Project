6b6ec6511e8986748c667068637d08b43bda1705
Saravanan <msx3gamer1@gmail.com>
2026-04-10 22:40:27 +0530

    Update database connection path and standardize table name; clear student records before insertion

--- sqlite.py
+++ sqlite.py
@@ -1,15 +1,17 @@
 import sqlite3
 
-connection=sqlite3.connect('studentdb.db')
+connection = sqlite3.connect(r"D:\GenAI\Gemini Pro Project\studentdb.db")
 
 cursor=connection.cursor()
 
 table_info="""
-CREATE TABLE IF NOT EXISTS students(NAME VARCHAR(20),CLASS VARCHAR(20),
+CREATE TABLE IF NOT EXISTS STUDENTS(NAME VARCHAR(20),CLASS VARCHAR(20),
 AGE INT);
 """
 
 cursor.execute(table_info)
+
+cursor.execute("DELETE FROM students")
 
 cursor.execute('''INSERT INTO students VALUES("John","10th",15)''')
 cursor.execute('''INSERT INTO students VALUES("Alice","9th",14)''')
@@ -17,11 +19,10 @@ cursor.execute('''INSERT INTO students VALUES("Bob","11th",16)''')
 cursor.execute('''INSERT INTO students VALUES("Eve","10th",15)''')
 cursor.execute('''INSERT INTO students VALUES("Charlie","9th",14)''')
 
+
+
 print("Data inserted are:")
 data=cursor.execute('''SELECT * FROM students''')
 for row in data:
     print(row)
     
-
-connection.commit()
-connection.close()  
